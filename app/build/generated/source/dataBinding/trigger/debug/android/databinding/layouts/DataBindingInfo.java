f921c92af0032b070ce248a546a43601a6a93f54
careyLi <careyli161128@sina.com>
2018-05-31 19:13:29 +0800

    base viewmode livedata room

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="47fc2a2e-1377-49b7-9180-6b556cad4e6f")
+public class DataBindingInfo {}
---
+++ app/build/generated/source/dataBinding/trigger/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="0cf1a020-8064-4ca4-9f71-f54cee69182b")
+public class DataBindingInfo {}
